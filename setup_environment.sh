62bed0f41bd186afa3f8375fef5982434f72e1cb
Malini Das <mdas@mozilla.com>
2014-02-12 18:31:33 -0500

    add a basic setup script for virtualenv and app installation

---
+++ setup_environment.sh
@@ -0,0 +1,23 @@
+# run with `source setup_environment`
+echo "Setting up virtualenv"
+which pip
+if [ $? != 0 ]; then
+  which easy_install
+  if [ $? != 0 ]; then
+    echo "You need to have easy_install on your path"
+    exit 0
+  fi
+  easy_install pip
+fi
+if [ ! -d "cert_venv" ]; then
+  pip install virtualenv
+  virtualenv --no-site-packages cert_venv
+fi
+source cert_venv/bin/activate
+pip install marionette_client
+pip install tornado
+echo "Installing CertTest app on phone"
+pushd device_setup
+python device_setup.py
+popd
+echo "Done"
