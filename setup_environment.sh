cf945089de36dd29dbf2a5c534c664dda001be99
Malini Das <mdas@mozilla.com>
2014-03-04 19:23:37 -0500

    marionette extension installation has moved to https://github.com/mozilla-b2g/fxos-certsuite

--- setup_environment.sh
+++ setup_environment.sh
@@ -19,10 +19,6 @@ pip install marionette_client
 pip install tornado
 echo "Installing CertTest app on phone"
 pushd device_setup
-./push_bundles.sh
-if [ $? != 0 ]; then
-  exit 1
-fi
 python device_setup.py
 popd
 echo "Done"
